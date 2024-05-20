4e79845ef7e39c69c6d471afbc6e1deedd7e6263
NamitSingh <38458966+namitsingh09@users.noreply.github.com>
2024-05-20 18:09:12 +0530

    Added Cv Link

--- app/About/page.js
+++ app/About/page.js
@@ -6,6 +6,7 @@ import Kss from "../About/Img.jpg"
 import Image from 'next/image'
 import Notification from './Notification'
 
+import Link from 'next/link'
 import ReadMoreLess from '../Componenets/Readmore'
 
 
@@ -66,12 +67,19 @@ export default function page() {
 					<path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
 				</svg>
 			</a>
+		
 		</div>
 	</div>
 </div>
 
+
         </div>
         <div className="grid gap-10 row-gap-1 sm:grid-cols-1" style={{textAlign:'center'}}>
+		<div class="flex justify-center">
+  <button  class="rounded-md bg-gradient-to-r from-black-500 h-10 to-blue-500 px-4 py-2 text-white hover:from-pink-500 hover:to-yellow-500">
+   <Link  href="https://drive.google.com/file/d/1DwzvThh_ZmDpDt97ekkrTaehpOU2JE7t/view?usp=sharing"> Download CV </Link>
+  </button>
+</div>
          
           
           <div className="max-w-full">
