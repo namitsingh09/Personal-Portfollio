5219da2770facba159fc08cf1b9c9211b5c8d84b
NamitSingh <38458966+namitsingh09@users.noreply.github.com>
2024-06-11 20:34:37 +0530

    added tabs

--- app/Componenets/Worke.js
+++ app/Componenets/Worke.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Notification from '../About/Notification'
 import Socio from './Socio'
@@ -7,7 +8,16 @@ import Nike from "../Work/nike.png"
 import Port from "../Work/port.png"
 import Ports from "../Work/pet.png"
 
+import { useState } from 'react'
+import Reacts from './Reactjs/React'
 export default function Worke() {
+    const [activeTab, setActiveTab] = useState('London');
+
+    const handleTabClick = (cityName) => {
+      setActiveTab(cityName);
+    };
+  
+      
   return (
   <div className='headers'>
   
@@ -37,7 +47,83 @@ export default function Worke() {
 
 
 
+<div className="container mx-auto justify-center px-4 py-6">
+      
 
+      <div className="tab overflow-hidden flex justify-center  max-w-full  bg-black-200">
+        <button
+          className={`tablinks  px-4 py-2 transition duration-300 text-lg ${
+            activeTab === 'London' ? 'bg-gray-300' : 'bg-inherit hover:bg-gray-400'
+          }`}
+          onClick={() => handleTabClick('London')}
+        >
+          London
+        </button>
+        <button
+          className={`tablinks px-4 py-2 transition duration-300 text-lg ${
+            activeTab === 'Paris' ? 'bg-gray-300' : 'bg-inherit hover:bg-gray-400'
+          }`}
+          onClick={() => handleTabClick('Paris')}
+        >
+          Paris
+        </button>
+        <button
+          className={`tablinks px-4 py-2 transition duration-300 text-lg ${
+            activeTab === 'Tokyo' ? 'bg-gray-300' : 'bg-inherit hover:bg-gray-400'
+          }`}
+          onClick={() => handleTabClick('Tokyo')}
+        >
+          Tokyo
+        </button>
+      </div>
+
+      <div
+        id="London"
+        className={`tabcontent p-4   ${
+          activeTab === 'London' ? 'block' : 'hidden'
+        }`}
+      >
+        <a href="https://venerable-chaja-5625ef.netlify.app/"
+                class="group relative w-60 flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                <Image src={Abu} loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-80">
+                </div>
+
+                <span class="relative ml-4 mb-3 inline-block text-sm text-white  md:ml-5 md:text-lg">ABUSTORE</span>
+                <button class="absolute inset-0 flex items-center justify-center text-white opacity-0 group-hover:opacity-100" onclick="navigateToLink('/About')">
+               
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+            </button>
+            <div class="absolute inset-0 bg-gradient-to-b from-black opacity-0 group-hover:opacity-40"></div>
+            </a>
+      </div>
+
+      <div
+        id="Paris"
+        className={`tabcontent p-4 border border-gray-300 border-t-0 ${
+          activeTab === 'Paris' ? 'block' : 'hidden'
+        }`}
+      >
+        <h3 className="text-xl font-bold mb-2">Paris</h3>
+        <p>Paris is the capital of France.</p>
+      </div>
+
+      <div
+        id="Tokyo"
+        className={`tabcontent p-4 border border-gray-300 border-t-0 ${
+          activeTab === 'Tokyo' ? 'block' : 'hidden'
+        }`}
+      >
+        <h3 className="text-xl font-bold mb-2">Tokyo</h3>
+        <p>Tokyo is the capital of Japan.</p>
+      </div>
+    </div>
 
 
 
